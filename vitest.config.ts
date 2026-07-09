c8157ea86efc883a51e43a849ff4c4757abed196
np6666 <1162323144@qq.com>
2026-07-09 18:08:27 +0800

    test: add coverage config

--- vitest.config.ts
+++ vitest.config.ts
@@ -4,12 +4,19 @@ export default defineConfig({
   test: {
     coverage: {
       provider: 'v8',
-      reporter: [
-        'text',
-        'html',
-        'json'
-      ],
-      reportsDirectory: './coverage'
+      reporter: ['text', 'html', 'json'],
+      reportsDirectory: './coverage',
+      exclude: [
+        'dist/**',
+        'coverage/**',
+        'examples/**',
+        'test/**',
+        '**/*.d.ts',
+        'src/index.ts',
+        'src/**/index.ts',
+        'src/types/**',
+        'src/plugins/Plugin.ts'
+      ]
     }
   }
 })
